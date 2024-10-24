9033ca1cfe1636728675fbf65f5477614d52f261
Marcelo Lopes <marc01mcl@hotmail.com>
2024-10-24 13:56:14 -0300

    regras de autorização

---
+++ Dominio/ModelViews/Home.cs
@@ -0,0 +1,8 @@
+namespace minimal_api.Dominio.ModelViews
+{
+    public struct Home
+    {
+        public readonly string Mensagem { get => "Bem Vindo a API de veiculos - Minimal API"; }
+        public readonly string Doc { get => "http://localhost:5159/swagger"; }
+    }
+}
